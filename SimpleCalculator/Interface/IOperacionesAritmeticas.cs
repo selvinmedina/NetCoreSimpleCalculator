6b798f0df4840b1cf3b6a58cb5e6a36696267537
selvinmedina <selvinmedina0016@gmail.com>
2020-06-24 12:26:19 -0600

    Agregar archivos de proyecto.

---
+++ SimpleCalculator/Interface/IOperacionesAritmeticas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleCalculator.Interface
+{
+    interface IOperacionesAritmeticas
+    {
+        public decimal Sumar(decimal num1, decimal num2);
+        public decimal Restar(decimal num1, decimal num2);
+        public decimal Multiplicar(decimal num1, decimal num2);
+        public decimal Dividir(decimal num1, decimal num2);
+
+    }
+}
